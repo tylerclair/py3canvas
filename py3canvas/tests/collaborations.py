1257674bb9913b843466e484e6b7bff783ca2434
Tyler Clair <tyler.clair@gmail.com>
2017-07-07 11:18:18 -0600

    changed package name to py3canvas to prevent import issues

---
+++ py3canvas/tests/collaborations.py
@@ -0,0 +1,35 @@
+"""Collaborations API Tests for Version 1.0.
+
+This is a testing template for the generated CollaborationsAPI Class.
+"""
+import unittest
+import requests
+import secrets
+from py3canvas.apis.collaborations import CollaborationsAPI
+from py3canvas.apis.collaborations import Collaborator
+from py3canvas.apis.collaborations import Collaboration
+
+
+class TestCollaborationsAPI(unittest.TestCase):
+    """Tests for the CollaborationsAPI."""
+
+    def setUp(self):
+        self.client = CollaborationsAPI(secrets.instance_address, secrets.access_token)
+
+    def test_list_collaborations_courses(self):
+        """Integration test for the CollaborationsAPI.list_collaborations_courses method."""
+        course_id = None  # Change me!!
+
+        r = self.client.list_collaborations_courses(course_id)
+
+    def test_list_collaborations_groups(self):
+        """Integration test for the CollaborationsAPI.list_collaborations_groups method."""
+        group_id = None  # Change me!!
+
+        r = self.client.list_collaborations_groups(group_id)
+
+    def test_list_members_of_collaboration(self):
+        """Integration test for the CollaborationsAPI.list_members_of_collaboration method."""
+        id = None  # Change me!!
+
+        r = self.client.list_members_of_collaboration(id, include=None)
